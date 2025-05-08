680ac82f62ea079afecd658ab1844f26b8b2c527
Shuichi Sasahara <114373114+sasashu1229@users.noreply.github.com>
2025-05-08 12:22:04 +0900

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "api_token" {
+  description = "API Token for Instana Access"
+  default     = "AqViYZxsSNayAc4WWC5Oxg"
+}
+
+variable "endpoint" {
+  description = "Instana Backend"
+  default     = "ibmdevsandbox-instanaibm.instana.io"
+}
