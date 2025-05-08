e5673b0d0f3bac8ad85e2400342f98fc21bc1474
Shuichi Sasahara <114373114+sasashu1229@users.noreply.github.com>
2025-05-08 12:27:23 +0900

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "api_token" {
-  description = "API Token for Instana Access"
-  default     = "AqViYZxsSNayAc4WWC5Oxg"
-}
-
-variable "endpoint" {
-  description = "Instana Backend"
-  default     = "ibmdevsandbox-instanaibm.instana.io"
-}
